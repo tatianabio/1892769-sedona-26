57af5dd2c5b9bbc89e36662c4d8353170da92280
tatianabio <105217283+tatianabio@users.noreply.github.com>
2022-09-02 15:01:30 +0300

    Добавляет оптимизацию svg и создание спрайта

--- gulpfile.js
+++ gulpfile.js
@@ -38,16 +38,21 @@ export const html = () => {
 // Scripts
 
 export const scripts = () => {
-  return gulp.src("source/js/*.js").pipe(terser()).pipe(gulp.dest("build/js"));
+  return gulp.src("source/js/*.js")
+    .pipe(terser())
+    .pipe(gulp.dest("build/js"));
 };
 
 // Images
 const optimizeImages = () => {
-  return gulp.src("source/img/**/*.{jpg,png}").pipe(squoosh()).pipe(gulp.dest("build/img"));
+  return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(squoosh())
+    .pipe(gulp.dest("build/img"));
 };
 
 const copyImages = () => {
-  return gulp.src("source/img/**/*.{jpg,png}").pipe(gulp.dest("build/img"));
+  return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(gulp.dest("build/img"));
 };
 
 // WebP
@@ -59,6 +64,22 @@ export const createWebP = () => {
 };
 
 // SVG
+
+export const svg = () => {
+  return gulp.src(["source/img/**/*.svg", "!source/img/icons/sprite/*.svg"])
+    .pipe(svgo())
+    .pipe(gulp.dest("build/img/"));
+};
+
+export const sprite = () => {
+  return gulp.src("source/img/icons/sprite/*.svg")
+    .pipe(svgo())
+    .pipe(svgstore( { inlineSvg: true }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img/icons"));
+};
+
+// Server
 
 const server = (done) => {
   browser.init({
@@ -79,4 +100,10 @@ const watcher = () => {
   gulp.watch("source/*.html").on("change", browser.reload);
 };
 
-export default gulp.series(html, styles, scripts, server, watcher);
+export default gulp.series(
+  html,
+  styles,
+  scripts,
+  server,
+  watcher
+);
